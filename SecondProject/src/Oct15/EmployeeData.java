54f1c6121a402dff49c48861162cc64a3c6f5e01
neha <44253440+Neha090@users.noreply.github.com>
2020-10-16 18:53:50 +0530

    Update EmployeeData.java

--- SecondProject/src/Oct15/EmployeeData.java
+++ SecondProject/src/Oct15/EmployeeData.java
@@ -7,12 +7,53 @@ import java.io.*;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
-
 public class EmployeeData {
 	
+	void updateFile(Map<Integer,Emp> employeesList)
+	{
+		try 
+		{
+			FileWriter fWriter=new FileWriter("C:\\Users\\nsaini\\git\\EmployeeData\\SecondProject\\NewRecord.csv");
+			BufferedWriter writer = new BufferedWriter(fWriter);
+			
+			writer.write("EmployeeId,FirstName,LastName,Age,EmailId");
+			writer.newLine();
+			
+			for(Map.Entry<Integer,Emp> entry : employeesList.entrySet())
+				{
+					writer.write(entry.getKey()+","+entry.getValue().firstName+","+entry.getValue().lastName+","+entry.getValue().age+","+entry.getValue().emailId);
+					writer.newLine();
+				}
+			   writer.close();
+			}
+		
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		
+	}
+	
+	
+	
+	void printOverWritten(List<Employee> duplicateRecords)
+	{
+
+		System.out.println("Records Over Written :");
+		System.out.println();
+		System.out.println("EmployeeId,FirstName,LastName,Age,EmailId");
+		for(Employee emp:duplicateRecords)
+		{
+			System.out.println(emp.id+","+emp.firstName+","+emp.lastName+","+emp.age+","+emp.emailId);
+		}
+		
+	}
+		
+
 	
 	public static void main(String[] args)throws IOException
 	{
+		EmployeeData employee =new EmployeeData();
 		Map<Integer,Emp> employeesList=new TreeMap<>();
 		List<Employee> duplicateRecords =new ArrayList();
 		
@@ -20,6 +61,7 @@ public class EmployeeData {
 		
 		try
 		{
+			
 			BufferedReader buffer=new BufferedReader(new FileReader("C:\\Users\\nsaini\\Downloads\\Records.csv"));
 			readLine=buffer.readLine();
 			
@@ -37,31 +79,12 @@ public class EmployeeData {
 				   employeesList.put(Integer.parseInt(emp[0]),new Emp(emp[2],emp[4],emp[6],Integer.parseInt(emp[13])));
 			}	
 			
-			System.out.println("Records Over Written :");
-			System.out.println();
-			System.out.println("EmployeeId,FirstName,LastName,Age,EmailId");
 			
-			for(Employee emp:duplicateRecords)
-			{
-				System.out.println(emp.id+","+emp.firstName+","+emp.lastName+","+emp.age+","+emp.emailId);
-			}
-
-			FileWriter fWriter=new FileWriter("C:\\Users\\nsaini\\eclipse-workspace\\SecondProject\\NewRecord.csv");
-			BufferedWriter writer = new BufferedWriter(fWriter);
-			
-			writer.write("EmployeeId,FirstName,LastName,Age,EmailId");
-			writer.newLine();
+			employee.printOverWritten(duplicateRecords);
+			employee.updateFile(employeesList);
 			
 			
-			for(Map.Entry<Integer,Emp> entry : result.entrySet())
-			{
-				writer.write(entry.getKey()+","+entry.getValue().firstName+","+entry.getValue().lastName+","+entry.getValue().age+","+entry.getValue().emailId);
-				writer.newLine();
-			}
-			
-			writer.close();
-			
-	}
+	    }
 		
 		catch(Exception e)
 		{
@@ -69,7 +92,6 @@ public class EmployeeData {
 		}
 		
    }
-	
 	
 }
 
